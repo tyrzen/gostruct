03a9b870909b0403e283f17894c3cb528925b235
delveper <yevhen@bilyk.dev>
2023-04-22 11:00:30 +0300

    feat: added set struct and make from html

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/delveper/gostruct
 
 go 1.20
+
+require (
+	github.com/antchfx/htmlquery v1.3.0
+	golang.org/x/net v0.9.0
+)
+
+require (
+	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- gostruct.go
+++ gostruct.go
@@ -3,8 +3,13 @@ package gostruct
 
 import (
 	"fmt"
+	"path"
 	"reflect"
 	"regexp"
+	"strings"
+
+	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html"
 )
 
 // MapStructFieldTags takes a generic type T and a string key.
@@ -42,4 +47,108 @@ func GetTagValue(tag reflect.StructTag, key string) (string, bool) {
 	}
 
 	return "", false
+}
+
+// SetField sets the value of a struct to a field.
+func SetField(fieldName string, dst any, value any) error {
+	fieldVal := reflect.ValueOf(dst).Elem().FieldByName(fieldName)
+
+	src := reflect.ValueOf(value)
+	if !src.Type().AssignableTo(fieldVal.Type()) {
+		return fmt.Errorf("cannot assign %v to %v", src.Type(), fieldVal.Type())
+	}
+
+	fieldVal.Set(src)
+
+	return nil
+}
+
+func MakeFromHTMLNode[T any](doc *html.Node) (T, error) {
+	var ent T
+	entFields := MapStructFieldTags[T]("xpath")
+
+	for field, tag := range entFields {
+		if tag == "-" {
+			continue
+		}
+
+		if node := htmlquery.FindOne(doc, tag); node != nil {
+			if val := htmlquery.InnerText(node); val != "" {
+				val = strings.TrimSpace(val)
+
+				if strings.Contains(field, "ID") {
+					val = path.Base(val)
+				}
+
+				if err := SetField(field, &ent, val); err != nil {
+					return *new(T), fmt.Errorf("setting field %v: %w", field, err)
+				}
+			}
+		}
+	}
+
+	return ent, nil
+}
+
+func MakeManyFromHTMLNode[T any](doc *html.Node, sel string) ([]T, error) {
+	entFields := MapStructFieldTags[T]("xpath")
+
+	many := make([]T, 0)
+
+	for _, ancestor := range htmlquery.Find(doc, sel) {
+		var one T
+
+		for field, tag := range entFields {
+			if tag == "-" {
+				continue
+			}
+
+			if node := htmlquery.FindOne(ancestor, tag); node != nil {
+				if val := htmlquery.InnerText(node); val != "" {
+					val = strings.TrimSpace(val)
+
+					if strings.Contains(field, "ID") {
+						val = path.Base(val)
+					}
+
+					if err := SetField(field, &one, val); err != nil {
+						return nil, fmt.Errorf("setting field %v: %w", field, err)
+					}
+				}
+			}
+
+		}
+
+		many = append(many, one)
+	}
+
+	return many, nil
+}
+
+func MakeFromHTMLText[T any](html string) (T, error) {
+	doc, err := htmlquery.Parse(strings.NewReader(html))
+	if err != nil {
+		return *new(T), fmt.Errorf("parsing html: %w", err)
+	}
+
+	entity, err := MakeFromHTMLNode[T](doc)
+	if err != nil {
+		return *new(T), fmt.Errorf("parsing html doc: %w", err)
+	}
+
+	return entity, nil
+}
+
+func MakeManyFromHTMLText[T any](html, sel string) ([]T, error) {
+	doc, err := htmlquery.Parse(strings.NewReader(html))
+	if err != nil {
+		return nil, fmt.Errorf("parsing html: %w", err)
+	}
+
+	many, err := MakeManyFromHTMLNode[T](doc, sel)
+	if err != nil {
+		return nil, fmt.Errorf("parsing html doc: %w", err)
+	}
+
+	return many, nil
 }
